56f50a43ef3f414f73a57414606d86ed238f5577
Matheus Oliveira <matheusoliveira.unb@gmail.com>
2025-06-22 14:28:40 -0300

    Added timer when click doesn't click.

--- UI_finder.py
+++ UI_finder.py
@@ -2,6 +2,7 @@ import pydirectinput  # Only for games (DirectX, etc)
 import pyautogui
 import os , sys
 from time import sleep
+from timeit import default_timer as timer
 import numpy as np
 
 pyautogui.MINIMUM_DURATION = 0
@@ -30,9 +31,10 @@ def human_mouse_movement(start, end, duration=1.0, steps=1000):
 
 def move_mouse_to_image(img, confidence=0.9, duration=1.0):
     try:
+        offset = 25
         button_to_press = pyautogui.locateCenterOnScreen(img, confidence=confidence)
         mouse_pos = pyautogui.position()
-        button_end_point = (np.random.uniform(button_to_press[0]-10, button_to_press[0]+10), np.random.uniform(button_to_press[1]-10, button_to_press[1]+10))
+        button_end_point = (np.random.uniform(button_to_press[0]-offset, button_to_press[0]+offset), np.random.uniform(button_to_press[1]-offset, button_to_press[1]+offset))
         human_mouse_movement(mouse_pos, button_end_point, duration=duration)
     except pyautogui.ImageNotFoundException:
         print("Sem imagem")
@@ -46,7 +48,7 @@ def is_image_on_screen(img, confidence=0.9, grayscale=False):
 
 def human_click(button='left', trained=False):
     if trained:
-        human_reaction_time = (0.190, 0.210)
+        human_reaction_time = (0.190, 0.215)
     else:
         human_reaction_time = (0.210, 0.230)
     reaction_time = np.random.uniform(human_reaction_time[0], human_reaction_time[1])
@@ -64,11 +66,12 @@ def main():
     error = 0
     is_trained = True
     catch = False
-    finish_after = 71
+    finish_after = 8
 
     try:
         move_mouse_to_image(cast_img, confidence=0.75, duration=np.random.uniform(0.6, 1.0))
         while True:
+            start_time = timer()
             if is_image_on_screen(cast_img, confidence=0.70):
                 if not catch and fishing_times != 0:
                     print("ERRO. NÃO PEGUEI  PEIXE :(")
@@ -76,20 +79,26 @@ def main():
                 if fishing_times >= finish_after or error >= 3:
                     sys.exit(0)
                 else:
-                    print(f"doing more {finish_after - fishing_times} tries")                
+                    print(f"doing more {finish_after - fishing_times} tries")
+                    if np.random.randint(1,100) > 83:
+                        move_mouse_to_image(cast_img, duration=np.random.uniform(0.2, 0.4))
                 fishing_times += 1
                 catch = False
                 human_click()
                 sleep(np.random.uniform(0.1, 0.3))
 
                 while True:
-                    if is_image_on_screen(reel_img, confidence=0.75, grayscale=True):
+                    if is_image_on_screen(reel_img, confidence=0.80, grayscale=True):
                         human_click(trained=is_trained)
                         while not is_image_on_screen(cast_img, confidence=0.70) and is_image_on_screen(reel_img, confidence=0.60, grayscale=True):
                             human_click(trained=is_trained)
                             sleep(np.random.uniform(0.07, 0.1))
                         catch = True
                         break
+                    elapsed_time = timer() - start_time
+                    if elapsed_time > 10:
+                        human_click()
+                        start_time = timer()
             
     except KeyboardInterrupt:
         sys.exit(0)
